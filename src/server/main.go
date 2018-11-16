682a8707ce045d7bd8a6c16eb95d69df856e32c6
Lee Graham <graham.la3@gmail.com>
2018-11-15 22:41:42 -0700

    Temporarily deactivate broken template route

--- src/server/main.go
+++ src/server/main.go
@@ -3,7 +3,7 @@ package main
 import (
   "fmt"
   "net/http"
-  "html/template"
+  // "html/template"
 )
 
 func ping(w http.ResponseWriter, r *http.Request) {
@@ -15,22 +15,22 @@ func element(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "<h1>this shit is confusing but I'll figure it out</h1><p>testing multiples</p><input type='email' /><input tupe='submit'/>")
 }
 
-type NewOut struct {
-  Name string
-  Age string
-}
+// type NewOut struct {
+//   Name string
+//   Age string
+// }
 
-func objectFunc(w http.ResponseWriter, r *http.Request) {
+// func objectFunc(w http.ResponseWriter, r *http.Request) {
 
-  out := NewOut{Name:"Lee", Age: "Never"}
-  data := NewOut{Name:"Lee", Age: "Never"}
-  tmpl, err := template.New("name").Parse("<h1>NOPE</h1>")
-  // Error checking elided
-  err = tmpl.Execute(out, data)
-}
+//   out := NewOut{Name:"Lee", Age: "Never"}
+//   data := NewOut{Name:"Lee", Age: "Never"}
+//   tmpl, err := template.New("name").Parse("<h1>NOPE</h1>")
+//   // Error checking elided
+//   err = tmpl.Execute(out, data)
+// }
 
 func main() {
-  http.HandleFunc("/template", objectFunc)
+  // http.HandleFunc("/template", objectFunc)
   http.Handle("/", http.FileServer(http.Dir("./src/server")))
   http.HandleFunc("/ping", ping)
   http.HandleFunc("/element", element)
